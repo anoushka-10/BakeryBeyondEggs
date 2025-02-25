20fcbb536c0557fbca8d82a8722bdfef160b74b6
Anoushka Goel <125151652+anoushka-10@users.noreply.github.com>
2025-02-25 19:34:45 +0530

    Update Login.js

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
 
     try {
       // Make the login request to the backend
-      const response = await axios.post("http://localhost:8093/login", {
+      const response = await axios.post("${process.env.REACT_APP_API_URL}/login", {
         username,
         password,
     }, {
